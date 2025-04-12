5d94f32606e35d20a3ed58d2e3b62357ceea66db
izzat1998 <izzattilla706@gmail.com>
2025-04-12 11:49:15 +0500

    Add navigation.js placeholder and document mobile menu implementation location

---
+++ static/js/components/navigation.js
@@ -0,0 +1,3 @@
+// Navigation functionality is handled in main.js
+// This file is kept as a placeholder for potential future modularization
+// See main.js lines 4-50 for the mobile menu implementation
